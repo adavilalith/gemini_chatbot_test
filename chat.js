f1ecf641afa3889f362fdfd4abc9316b1f1d7b20
Adavi Lalith <adavilalith11@gmail.com>
2024-04-21 20:39:30 +0530

    starting work with gemini and added api keysto gitignore

---
+++ chat.js
@@ -0,0 +1,9 @@
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+addEventListener('submit',(e)=>{
+    const API_KEY =
+    e.preventDefault()
+    let prompt = document.getElementById('prompt') 
+    console.log(prompt.value)
+})
