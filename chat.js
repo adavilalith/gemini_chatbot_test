f6aa3108f021bcb3c8339a739865977149c8d66a
Adavi Lalith <adavilalith11@gmail.com>
2024-04-21 20:48:32 +0530

    cannot use gemini api in vanilla js shifting to react

--- chat.js
+++
@@ -1,9 +0,0 @@
-
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
-addEventListener('submit',(e)=>{
-    const API_KEY =
-    e.preventDefault()
-    let prompt = document.getElementById('prompt') 
-    console.log(prompt.value)
-})
